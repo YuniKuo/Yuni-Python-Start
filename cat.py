f09a1e0ef3dfdaab089e9e10c29d5b65a16630c8
YuniKuo <stich513327@gmail.com>
2025-05-17 15:43:38 +1000

    My First Python Game : 『Cat Interaction Game』
    This is my very first mini game written in Python.
    The main character is my cat ' Pang Pang' (胖胖) meow~
    The features include name input, interactive dialogue , and conditional branching.
    -
    這是我第一個用Python寫的小遊戲。
    遊戲主角是我的貓咪「胖胖」喵，功能包含：輸入名字、問答互動、條件判斷等。

---
+++ cat.py
@@ -0,0 +1,20 @@
+name = '胖胖'
+print (f"喵～我的媽咪都叫我{name}喵～")
+print ('你是媽咪的朋友嗎～喵？你的名字是什麼呀？')
+
+your_name = input ('跟我說喵～～')
+
+print(f"原來你叫做{your_name}呀喵～")
+print('以後就多多指教了喵～')
+
+print(f"{your_name}聽媽咪說～你們家也有養貓咪是嗎喵～～？")
+your_cat = input ('如果有的話～跟我說『好呀』讓我知道好嗎～！')
+
+if your_cat == '好呀':
+    your_cat_name = input('好幸運呀你！跟我說說他的名字好喵')
+    print(f"原來他叫做{your_cat_name}呀～聽起來跟我一樣可愛耶")
+else:
+    print('原來你沒有養貓呀，那你可以隨時來跟我玩喵！')
+        
+
+
