ccab7d4a378376137e235ff8b64f3233c4817915
YuniKuo <stich513327@gmail.com>
2025-05-18 14:37:39 +1000

    Upload two Python practice scripts
    - 02-type-checking.py：
    練習資料型別與 f-string 輸出  
     Basic data types & f-string
    
    - 03-type-casting.py：
    練習 int / float / str / bool 型別轉換
    Type conversion among int/float/str

---
+++ 02-type-checking.py
@@ -0,0 +1,21 @@
+weight = 50
+print (f"我的體重是 {weight} 公斤")
+print (f"My body weight is {weight}kg.")
+print ( type(weight))
+
+chocolate = 3.27
+print (f"做一個巧可力蛋糕需要 {chocolate} 公克的巧克力")
+print (f"To make a chocolate cake, you need {chocolate} grams of chocolate.")
+
+print ( type (chocolate) )
+
+name = 'Yuni'
+print (f"哈囉，我的名字是{name}喔")
+print (f"Hello, My name is {name}!")
+print (type (name))
+
+
+beginner = True
+print (f"Yuni 是一個自學初學者{beginner}.")
+print (f"Yuni is a self-taught beginner {beginner}")
+print (type (beginner))
---
+++ 03-type-casting.py
@@ -0,0 +1,29 @@
+#type_casting
+
+weight = 50
+print (weight)
+print (type(weight))
+
+weight = float(weight)
+print (weight)
+print(type(weight))
+
+#type_casting
+
+chocolate = 3.27
+print (chocolate)
+print (type(chocolate))
+
+chocolate = int(chocolate)
+print (chocolate)
+print (type (chocolate))
+
+#type_casting
+
+beginner = True
+print (beginner)
+print (type(beginner))
+
+beginner = str(beginner)
+print (beginner)
+print (type(beginner))
